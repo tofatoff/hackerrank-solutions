318b8a5b6d29eae98173f031bfbb64ce373ae6b9
tofatoff <69616242+tofatoff@users.noreply.github.com>
2021-12-01 10:55:01 +0700

    Create 30daysProblemSolving_day15-linkedList.py

---
+++ 30daysProblemSolving_day15-linkedList.py
@@ -0,0 +1,13 @@
+
+
+    def insert(self,head,data): 
+    #Complete this method
+        if head == None:
+            return Node(data)
+        elif head.next == None:
+            head.next = Node(data)
+        else:
+            self.insert(head.next,data)
+            
+        return head
+
